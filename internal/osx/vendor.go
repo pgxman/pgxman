4f19266462ecc80cf2f743148134651b91222184
Owen Ou <o@owenou.com>
2023-06-23 12:27:49 -0700

    Fallback to `runtime.GOOS` for unsupported OS

--- internal/osx/vendor.go
+++ internal/osx/vendor.go
@@ -1,10 +1,19 @@
 package osx
 
-import "github.com/zcalusic/sysinfo"
+import (
+	"runtime"
+
+	"github.com/zcalusic/sysinfo"
+)
 
 func Vendor() string {
 	var si sysinfo.SysInfo
 	si.GetSysInfo()
 
-	return si.OS.Vendor
+	os := si.OS.Vendor
+	if os == "" {
+		os = runtime.GOOS
+	}
+
+	return os
 }
