f2418022bfb631f09fb2e5c7c36acf62fe359b5f
Owen Ou <169064+owenthereal@users.noreply.github.com>
2023-07-25 13:59:47 -0700

    Support runtime dependencies from another apt repo (#17)
    * git clone buildkit when `pgxman update`
    
    * Support runtime dependencies from another apt repos

--- internal/plugin/debian/updater.go
+++ internal/plugin/debian/updater.go
@@ -2,9 +2,13 @@ package debian
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/pgxman/pgxman"
 	"github.com/pgxman/pgxman/internal/log"
+	"golang.org/x/exp/slog"
 )
 
 const (
@@ -17,7 +21,13 @@ type DebianUpdater struct {
 }
 
 func (u *DebianUpdater) Update(ctx context.Context) error {
-	return addAptRepos(
+	u.Logger.Debug("Downloading buildkit source", slog.String("dir", BuildkitDir))
+	if err := downloadBuildkitSource(ctx); err != nil {
+		return errBuildkitSource{Err: err}
+	}
+
+	u.Logger.Debug("Adding apt repositories")
+	if err := addAptRepos(
 		ctx,
 		[]pgxman.AptRepository{
 			{
@@ -37,5 +47,42 @@ func (u *DebianUpdater) Update(ctx context.Context) error {
 			},
 		},
 		u.Logger,
-	)
+	); err != nil {
+		return fmt.Errorf("add apt repos: %w", err)
+	}
+
+	return nil
+}
+
+func downloadBuildkitSource(ctx context.Context) error {
+	if err := os.MkdirAll(ConfigDir, 0755); err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(BuildkitDir); err == nil {
+		gitFetchCmd := exec.CommandContext(ctx, "git", "fetch", "origin")
+		gitFetchCmd.Dir = BuildkitDir
+
+		if out, err := gitFetchCmd.CombinedOutput(); err != nil {
+			return fmt.Errorf("git fetch: %w\n%s", err, out)
+		}
+
+		gitResetCmd := exec.CommandContext(ctx, "git", "reset", "--hard", "origin/main")
+		gitResetCmd.Dir = BuildkitDir
+
+		if out, err := gitResetCmd.CombinedOutput(); err != nil {
+			return fmt.Errorf("git reset: %w\n%s", err, out)
+		}
+
+		return nil
+	} else {
+		gitCloneCmd := exec.CommandContext(ctx, "git", "clone", "--single-branch", "https://github.com/pgxman/buildkit.git")
+		gitCloneCmd.Dir = ConfigDir
+
+		if out, err := gitCloneCmd.CombinedOutput(); err != nil {
+			return fmt.Errorf("git clone: %w\n%s", err, out)
+		}
+	}
+
+	return nil
 }
