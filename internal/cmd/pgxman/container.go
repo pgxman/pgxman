ab9a7492bc73334bccab7e4faa891a3c32910667
Owen Ou <169064+owenthereal@users.noreply.github.com>
2023-11-07 12:13:31 -0800

    Update internal/cmd/pgxman/container.go
    Co-authored-by: Jonathan Dance (JD) <wuputah@users.noreply.github.com>

--- internal/cmd/pgxman/container.go
+++ internal/cmd/pgxman/container.go
@@ -146,7 +146,7 @@ func newContainerTeardownCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "teardown",
 		Short: "Tear down a container",
-		Long:  `Tear down a playground container and purge all data.`,
+		Long:  `Tear down a container and purge all data.`,
 		Example: ` # Tear down the PostgreSQL 15 playgrond container.
 pgxman container teardown pgxman_runner_15
 
