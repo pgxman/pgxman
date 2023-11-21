26f376993370c1a38453ff9ae7afa82fb32d96db
Owen Ou <169064+owenthereal@users.noreply.github.com>
2023-11-21 13:19:49 -0800

    Move doctor requirement to "Recommendation" section for `pgxman doctor` (#104)
    Move doctor requirement to "Recommendation" section for `pgxman doctor`

--- internal/doctor/doctor.go
+++ internal/doctor/doctor.go
@@ -11,33 +11,52 @@ import (
 	"github.com/pgxman/pgxman/internal/pg"
 )
 
-func Validate(ctx context.Context) []ValidationResult {
+func Validate(ctx context.Context) (required []ValidationResult, optional []ValidationResult) {
 	var (
-		results    []ValidationResult
-		validators = []Validator{&dockerValidator{}}
+		validators []Validator
 	)
+
 	if runtime.GOOS == "linux" {
 		validators = append(validators, &postgresValidator{})
 	}
+	validators = append(validators, &dockerValidator{})
 
 	for _, v := range validators {
-		results = append(results, v.Validate(ctx)...)
+		for _, r := range v.Validate(ctx) {
+			switch r.Category {
+			case ValidationCategoryRequired:
+				required = append(required, r)
+			case ValidationCategoryOptional:
+				optional = append(optional, r)
+			default:
+				panic(fmt.Sprintf("unknown validation result category: %s", r.Category))
+			}
+		}
 	}
 
-	return results
+	return required, optional
 }
 
 const (
-	ValidationSuccess ValidationType = "success"
-	ValiationError    ValidationType = "error"
+	ValidationSuccess ValidationResultType = "success"
+	ValidationWarning ValidationResultType = "warning"
+	ValiationError    ValidationResultType = "error"
 )
 
-type ValidationType string
+type ValidationResultType string
 
 type ValidationResult struct {
-	Type    ValidationType
-	Message string
+	Type     ValidationResultType
+	Category ValidationResultCategory
+	Message  string
 }
+
+const (
+	ValidationCategoryRequired ValidationResultCategory = "required"
+	ValidationCategoryOptional ValidationResultCategory = "optional"
+)
+
+type ValidationResultCategory string
 
 type Validator interface {
 	Validate(context.Context) []ValidationResult
@@ -48,14 +67,29 @@ type dockerValidator struct {
 
 func (v *dockerValidator) Validate(ctx context.Context) []ValidationResult {
 	var (
-		results           []ValidationResult
+		results  []ValidationResult
+		category ValidationResultCategory
+	)
+
+	switch runtime.GOOS {
+	case "linux":
+		category = ValidationCategoryOptional
+	case "darwin":
+		category = ValidationCategoryRequired
+	default:
+		category = ValidationCategoryRequired
+	}
+
+	var (
 		dockerIsInstalled = ValidationResult{
-			Type:    ValidationSuccess,
-			Message: "Docker is installed",
+			Type:     ValidationSuccess,
+			Category: category,
+			Message:  "Docker is installed",
 		}
 		dockerIsRunning = ValidationResult{
-			Type:    ValidationSuccess,
-			Message: `Docker daemon is running`,
+			Type:     ValidationSuccess,
+			Category: category,
+			Message:  `Docker daemon is running`,
 		}
 	)
 
@@ -63,62 +97,78 @@ func (v *dockerValidator) Validate(ctx context.Context) []ValidationResult {
 	if dockerErr != nil {
 		if errors.Is(dockerErr, docker.ErrDockerNotRunning) || errors.Is(dockerErr, docker.ErrDockerNotFound) {
 			if errors.Is(dockerErr, docker.ErrDockerNotFound) {
-				var lines []string
+				var (
+					lines      []string
+					resultType ValidationResultType
+				)
 				if runtime.GOOS == "linux" {
 					lines = []string{
 						"To use the `pgxman container` commands, you'll need to install Docker.",
 						"Visit https://docs.docker.com/engine/install for more info.",
 					}
+					resultType = ValidationWarning
 				} else if runtime.GOOS == "darwin" {
 					lines = []string{
 						"pgxman emulates the production experience on macOS.",
 						"To use the `pgxman install` & `pgxman container` commands, you'll need to install Docker.",
 						"Visit https://docs.docker.com/engine/install for more info.",
 					}
+					resultType = ValiationError
 				} else {
 					lines = []string{
 						"Visit https://docs.docker.com/engine/install for more info.",
 					}
+					resultType = ValiationError
+					category = ValidationCategoryRequired
 				}
 
 				results = append(results, ValidationResult{
-					Type:    ValiationError,
-					Message: "Docker is not installed\n" + addPrefixSpaces(lines, 4),
+					Type:     resultType,
+					Category: category,
+					Message:  "Docker is not installed\n" + addPrefixedSpaces(lines, 4),
 				})
 			} else {
 				results = append(results, dockerIsInstalled)
 			}
 
 			if errors.Is(dockerErr, docker.ErrDockerNotRunning) {
-				var lines []string
+				var (
+					lines      []string
+					resultType ValidationResultType
+				)
 				if runtime.GOOS == "linux" {
 					lines = []string{
 						"To use the `pgxman container` commands, you'll need to start the Docker daemon.",
 						"Visit https://docs.docker.com/config/daemon/start for more info.",
 					}
+					resultType = ValidationWarning
 				} else if runtime.GOOS == "darwin" {
 					lines = []string{
 						"pgxman emulates the production experience on macOS.",
 						"To use the `pgxman install` & `pgxman container` commands, you'll need to start the Docker daemon.",
 						"Visit https://docs.docker.com/config/daemon/start for more info.",
 					}
+					resultType = ValiationError
 				} else {
 					lines = []string{
 						"Visit https://docs.docker.com/config/daemon/start for more info.",
 					}
+					resultType = ValiationError
 				}
 
 				results = append(results, ValidationResult{
-					Type:    ValiationError,
-					Message: "Docker daemon is not running\n" + addPrefixSpaces(lines, 4),
+					Type:     resultType,
+					Category: category,
+					Message:  "Docker daemon is not running\n" + addPrefixedSpaces(lines, 4),
 				})
 			} else {
 				results = append(results, dockerIsRunning)
 			}
 		} else {
 			results = append(results, ValidationResult{
-				Type:    ValiationError,
-				Message: fmt.Sprintf("Docker error: %s", dockerErr.Error()),
+				Type:     ValiationError,
+				Category: category,
+				Message:  fmt.Sprintf("Docker error: %s", dockerErr.Error()),
 			})
 		}
 	} else {
@@ -147,20 +197,22 @@ func (v *postgresValidator) Validate(ctx context.Context) []ValidationResult {
 			"Visit https://www.postgresql.org/download for more info.",
 		}
 		results = append(results, ValidationResult{
-			Type:    ValiationError,
-			Message: "PostgreSQL is not installed\n" + addPrefixSpaces(lines, 4),
+			Type:     ValiationError,
+			Category: ValidationCategoryRequired,
+			Message:  "PostgreSQL is not installed\n" + addPrefixedSpaces(lines, 4),
 		})
 	} else {
 		results = append(results, ValidationResult{
-			Type:    ValidationSuccess,
-			Message: fmt.Sprintf("PostgreSQL %s is installed", pgVer),
+			Type:     ValidationSuccess,
+			Category: ValidationCategoryRequired,
+			Message:  fmt.Sprintf("PostgreSQL %s is installed", pgVer),
 		})
 	}
 
 	return results
 }
 
-func addPrefixSpaces(lines []string, spaces int) string {
+func addPrefixedSpaces(lines []string, spaces int) string {
 	var result []string
 	for _, line := range lines {
 		result = append(result, fmt.Sprintf("%s%s", strings.Repeat(" ", spaces), line))
