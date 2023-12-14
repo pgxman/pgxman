715f59e3bb6950b2f4ea33b9ee76d15a9c9aa567
Owen Ou <169064+owenthereal@users.noreply.github.com>
2023-12-14 09:09:35 -0800

    [HYD-753] Warn about non pgdg pg distro (#139)
    * Detect version returns error when pg distro is unsupported
    
    * pgxman doctor shows unsupported pg distro
    
    * Tweak pg warning message

---
+++ internal/pg/version_test.go
@@ -0,0 +1,53 @@
+package pg
+
+import (
+	"testing"
+
+	"github.com/pgxman/pgxman"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parsePGVersion(t *testing.T) {
+	cases := []struct {
+		Name      string
+		Str       string
+		WantPGVer pgxman.PGVersion
+		WantErr   error
+	}{
+		{
+			Name:      "happy path",
+			Str:       "PostgreSQL 16.1 (Debian 16.1-1.pgdg120+1)",
+			WantPGVer: pgxman.PGVersion16,
+			WantErr:   nil,
+		},
+		{
+			Name:      "unsupported pg distro",
+			Str:       "PostgreSQL 14.10 (Ubuntu 14.10-0ubuntu0.22.04.1)",
+			WantPGVer: pgxman.PGVersionUnknown,
+			WantErr:   ErrUnsupportedPGDistro,
+		},
+		{
+			Name:      "unsupported pg version",
+			Str:       "PostgreSQL 10.1 (Debian 16.1-1.pgdg120+1)",
+			WantPGVer: pgxman.PGVersionUnknown,
+			WantErr:   ErrUnsupportedPGVersion,
+		},
+		{
+			Name:      "malformed pg version",
+			Str:       "PostgreSQL 1234",
+			WantPGVer: pgxman.PGVersionUnknown,
+			WantErr:   ErrParsingPGVersion,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			ver, err := parsePGVersion(c.Str)
+			assert.Equal(c.WantPGVer, ver)
+			assert.Equal(c.WantErr, err)
+		})
+	}
+}
