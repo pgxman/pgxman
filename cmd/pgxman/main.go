326eb94cc994c49ee35971d274a07a7822d9d2da
Owen Ou <o@owenou.com>
2024-02-14 10:01:29 -0800

    Don't show `Error:` prefix from corbra

--- cmd/pgxman/main.go
+++ cmd/pgxman/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/pgxman/pgxman/internal/cmd/pgxman"
 )
 
+type exitCode int
+
+const (
+	exitOK    exitCode = 0
+	exitError exitCode = 1
+)
+
 func main() {
-	if err := pgxman.Execute(); err != nil {
-		os.Exit(1)
+	code := mainRun()
+	os.Exit(int(code))
+}
+
+func mainRun() exitCode {
+	ctx := context.Background()
+
+	cmd, err := pgxman.Execute(ctx)
+	if err != nil {
+		printError(os.Stderr, err)
+		return exitError
 	}
+
+	if !cmd.Runnable() {
+		return exitError
+	}
+
+	return exitOK
+}
+
+func printError(out io.Writer, err error) {
+	fmt.Fprintln(out, err)
 }
